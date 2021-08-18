af2837111b1eff36fa3b86213f1dbe978bd858fd
liamlkh <>
2021-08-19 02:07:35 +0800

    fix minor bugs

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,13 +6,21 @@ const Detail = ({ data }) => {
   const { id } = data
   const item = store.getState().threeDItems.data[id]
 
-  if (item.type == 'image') return (
-    <img className={`image-show ${item.images[0].width > item.images[0].height ? 'is-wide' : ''}`} src={item.images[0].url}/>
-  )
+  if (item.type == 'image') {
+    let url = item.images[0].url
+    url = url.startsWith('data') ? url : process.env.PUBLIC_URL + url
+    return (
+      <img className={`image-show ${item.images[0].width > item.images[0].height ? 'is-wide' : ''}`} src={url}/>
+    )
+  }
 
-  else if (item.type == 'video') return (
-    <video className="video-show" autoPlay src={item.url} controls/>
-  )
+  else if (item.type == 'video') {
+    let url = item.url
+    url = url.startsWith('data') ? url : process.env.PUBLIC_URL + url
+    return (
+      <video className="video-show" autoPlay src={url} controls/>
+    )
+  }
 }
 
 export default Detail
